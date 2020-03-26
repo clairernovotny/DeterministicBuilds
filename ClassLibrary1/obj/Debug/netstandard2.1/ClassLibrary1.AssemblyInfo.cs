1b4eca845a566773a8ebb5561e06ca8c3f445e08
Claire Novotny <claire@novotny.org>
2020-03-26 19:04:02 -0400

    foo

--- ClassLibrary1/obj/Debug/netstandard2.1/ClassLibrary1.AssemblyInfo.cs
+++ ClassLibrary1/obj/Debug/netstandard2.1/ClassLibrary1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompanyName")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15dafa203b7cc81e9329d1db4fe9287d27311be6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
